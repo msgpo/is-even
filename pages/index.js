93c9f9c18a1120af27cd40a016e031aabb86f8f5
Rishov Sarkar <ArkaneMoose@users.noreply.github.com>
2020-08-02 18:00:25 -0400

    feat: add homepage

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,26 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="min-h-screen flex flex-col justify-center items-center">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Is Even</title>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <h1>Is Even</h1>
+      <p>Your one-stop shop for all things &ldquo;even&rdquo;</p>
+      <form className="flex w-full max-w-5xl text-3xl mt-10 mb-12 rounded-full">
+        <input type="number" className="flex-1 transition ease-out duration-200 appearance-none border-4 rounded-l-full w-full py-5 px-10 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        placeholder="2, 3, 4, etc&hellip;" />
+        <button className="rounded-r-full py-5 px-10 bg-purple-700 text-white" type="submit">Tell me!</button>
+      </form>
+      <a className="italic text-blue-500 active:text-blue-800 underline" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Companion product isOdd coming soon! Follow our blog for the latest updates.</a>
+      <style jsx>{`
+        form:focus-within {
+          transform: translateY(-10px);
+          box-shadow: 0 25px 50px -12px rgba(0, 0, 0, 0.25);
+          transition: transform 0.2s, box-shadow 0.2s;
+        }
+      `}</style>
     </div>
   )
 }
